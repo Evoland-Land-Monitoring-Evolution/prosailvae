lr finder and major bug correction (nll computation)

dataset/loaders.py
@@ -142,9 +142,40 @@ def load_test_ids(file_prefix="s2_", data_dir=None):
                                          os.pardir),"data/")
     return torch.load(data_dir + f"/{file_prefix}test_ids.pt")
 
+def lr_finder_loader(sample_ids=None, 
+                     batch_size=1024, num_workers=0, file_prefix="s2_", 
+                     data_dir=None):
+    if data_dir is None:
+        data_dir = os.path.join(os.path.join(os.path.dirname(prosailvae.__file__),
+                                             os.pardir),"data/")
+    s2_refl = torch.load(data_dir + f"/{file_prefix}prosail_s2_sim_refl.pt")
+    len_dataset = s2_refl.size(0)
+    
+    prosail_sim_vars = torch.load(data_dir + f"/{file_prefix}prosail_sim_vars.pt")
+    angles = prosail_sim_vars[:,-3:]
+    if sample_ids is None:
+        sample_ids = torch.arange(0,len_dataset) 
+        sub_s2_refl = s2_refl
+        sub_angles = angles
+    else:
+        assert (sample_ids < len_dataset).all()
+        sub_s2_refl = s2_refl[sample_ids,:]
+        sub_angles = angles[sample_ids,:]
+
+    dataset = TensorDataset(torch.concat((sub_s2_refl.float(),
+                            sub_angles.float()), axis=1), 
+                            sub_s2_refl.float(),
+                            )
+
+    loader = DataLoader(dataset,
+                        batch_size=batch_size,
+                        num_workers=num_workers)
+    return loader
+    
 
 def get_simloader(valid_ratio=None, sample_ids=None, 
-                 batch_size=1024, num_workers=0, file_prefix="s2_", data_dir=None):
+                 batch_size=1024, num_workers=0, file_prefix="s2_", 
+                 data_dir=None, cat_angles=False):
     if data_dir is None:
         data_dir = os.path.join(os.path.join(os.path.dirname(prosailvae.__file__),
                                              os.pardir),"data/")
@@ -166,6 +197,7 @@ def get_simloader(valid_ratio=None, sample_ids=None,
         sub_angles = angles[sample_ids,:]
         
     if valid_ratio is None:
+       
         dataset = TensorDataset(sub_s2_refl.float(),
                                 sub_angles.float(), 
                                 sub_prosail_params.float(),
@@ -186,17 +218,18 @@ def get_simloader(valid_ratio=None, sample_ids=None,
         sub_s2_refl_valid = s2_refl[ids_valid,:]
         sub_prosail_params_valid = prosail_params[ids_valid,:]
         sub_angles_valid = angles[ids_valid,:]
-        
+
         train_dataset = TensorDataset(sub_s2_refl_train.float(),
                                       sub_angles_train.float(),
                                       sub_prosail_params_train.float(),)
-        train_loader = DataLoader(train_dataset,
-                            batch_size=batch_size,
-                            num_workers=num_workers)
         valid_dataset = TensorDataset(sub_s2_refl_valid.float(),
                                       sub_angles_valid.float(),
                                       sub_prosail_params_valid.float(),
                                        )
+        train_loader = DataLoader(train_dataset,
+                            batch_size=batch_size,
+                            num_workers=num_workers)
+        
         valid_loader = DataLoader(valid_dataset,
                             batch_size=batch_size,
                             num_workers=num_workers)

prosailvae/decoders.py
@@ -42,8 +42,13 @@ class ProsailSimulatorDecoder(Decoder):
         
         return rec
     
+    # def loss(self, tgt, rec):        
+    #     rec_err_var = torch.var(rec-tgt.unsqueeze(1), 1).unsqueeze(1)
+    #     rec_loss = gaussian_nll(tgt.unsqueeze(1), rec, rec_err_var, device=self.device).mean() 
+    #     return rec_loss
+    
     def loss(self, tgt, rec):        
-        rec_err_var = torch.var(rec-tgt.unsqueeze(1), 1).unsqueeze(1)
-        rec_loss = gaussian_nll(tgt.unsqueeze(1), rec, rec_err_var, device=self.device).mean() 
+        rec_err_var = torch.var(rec-tgt.unsqueeze(2), 2).unsqueeze(2)
+        rec_loss = gaussian_nll(tgt.unsqueeze(2), rec, rec_err_var, device=self.device).mean() 
         return rec_loss
 

prosailvae/simvae.py
@@ -89,8 +89,11 @@ class SimVAE(nn.Module):
         rec = self.decoder.decode(sim, dec_args)
         return rec
         
-    def forward(self, x, angles=None, n_samples=1):
+    def forward(self, x, angles=None, n_samples=20):
         # encoding
+        if angles is None:
+            angles = x[:,-3:]
+            x = x[:,:-3]
         y = self.encode(x, angles)
         dist_params = self.lat_space.get_params_from_encoder(y)
         
@@ -275,3 +278,23 @@ class SimVAE(nn.Module):
                 print("NaN Loss encountered during validation !")
         return valid_loss_dict
 
+
+def gaussian_nll(x, mu, sigma, eps=1e-6, device='cpu'):
+    eps = torch.tensor(eps).to(device)
+    return (torch.square(x - mu) / torch.max(sigma, eps)).mean(1).sum(1) +  \
+            torch.log(torch.max(sigma.squeeze(1), eps)).sum(1)
+from prosailvae.dist_utils import kl_tn_uniform     
+     
+def lr_finder_elbo(model_outputs, label, beta=1):
+    dist_params, z, sim, rec = model_outputs
+    rec_err_var = torch.var(rec-label.unsqueeze(2), 2).unsqueeze(2)
+    rec_loss = gaussian_nll(label.unsqueeze(2), rec, rec_err_var).mean() 
+    loss_sum=rec_loss
+    sigma = dist_params[:, :, 1].squeeze()
+    mu = dist_params[:, :, 0].squeeze()
+    kl = kl_tn_uniform(mu, sigma) 
+    
+    kl_loss = beta * kl.sum(1).mean()
+    loss_sum+=kl_loss
+
+    return loss_sum

prosailvae/train.py
@@ -6,13 +6,12 @@ Created on Mon Nov 14 14:20:44 2022
 @author: yoel
 """
 from prosailvae.prosail_vae import get_prosail_VAE
-from dataset.loaders import load_train_valid_ids, get_s2loader, get_simloader, get_norm_coefs
+from dataset.loaders import  get_simloader, get_norm_coefs
 # from prosailvae.ProsailSimus import get_ProsailVarsIntervalLen
 from metrics.metrics import get_metrics, save_metrics
 from metrics.prosail_plots import plot_metrics, plot_rec_and_latent, loss_curve, plot_param_dist, plot_pred_vs_tgt, plot_refl_dist, pair_plot
 from datetime import datetime 
 import torch.optim as optim
-import json
 import torch
 import pandas as pd
 import argparse
@@ -22,18 +21,9 @@ import prosailvae
 from tqdm import trange
 import warnings
 from time import sleep
-import numpy as np
-import matplotlib.pyplot as plt 
 from prosailvae.ProsailSimus import PROSAILVARS, BANDS
+from prosailvae.utils import load_dict, save_dict
 
-def save_dict(data_dict, dict_file_path):
-    with open(dict_file_path, 'w') as fp:
-        json.dump(data_dict, fp, indent=4)
-
-def load_dict(dict_file_path):
-    with open(dict_file_path, "r") as read_file:
-        data_dict = json.load(read_file)
-    return data_dict
 
 def check_fold_res_dir(fold_dir, n_xp, params):
     same_fold = ""
@@ -110,18 +100,18 @@ def get_prosailvae_train_parser():
        
     return parser
 
-def training_loop(phenoVAE, optimizer, n_epoch, train_loader, valid_loader, res_dir):
+def training_loop(phenoVAE, optimizer, n_epoch, train_loader, valid_loader, res_dir, n_samples=20):
     all_train_loss_df = pd.DataFrame()
     all_valid_loss_df = pd.DataFrame()
     best_val_loss = torch.inf
     for epoch in trange(n_epoch, desc='phenoVAE training', leave=True):
         try:
-            train_loss_dict = phenoVAE.fit(train_loader, optimizer, n_samples=10)
+            train_loss_dict = phenoVAE.fit(train_loader, optimizer, n_samples=n_samples)
         except:
             print(f"Error during Training at epoch {epoch} !")
             break
         try:
-            valid_loss_dict = phenoVAE.validate(train_loader, n_samples=10)
+            valid_loss_dict = phenoVAE.validate(train_loader, n_samples=n_samples)
         except:
             print(f"Error during Validation at epoch {epoch} !")
         train_loss_dict['epoch']=epoch
@@ -186,14 +176,15 @@ if __name__ == "__main__":
     
     optimizer = optim.Adam(prosail_VAE.parameters(), lr=params["lr"])
     # prosail_VAE.load_ae("/home/yoel/Documents/Dev/PROSAIL-VAE/prosailvae/results/" + "/prosailvae_weigths.tar", optimizer=optimizer)
-    
+
     # Training
     all_train_loss_df, all_valid_loss_df = training_loop(prosail_VAE, 
                                                             optimizer, 
                                                             params['epochs'],
                                                             train_loader, 
                                                             valid_loader,
-                                                            res_dir=res_dir) 
+                                                            res_dir=res_dir,
+                                                            n_samples=params["n_samples"]) 
     # Saving Loss
     loss_dir = res_dir + "/loss/"
     os.makedirs(loss_dir)
@@ -220,6 +211,7 @@ if __name__ == "__main__":
     
     # Plotting results
     metrics_dir = res_dir + "/metrics_plot/"
+    os.makedirs(metrics_dir)
     plot_metrics(metrics_dir, alpha_pi, maer, mpiwr, picp, mare)
     rec_dir = res_dir + "/reconstruction/"
     os.makedirs(rec_dir)
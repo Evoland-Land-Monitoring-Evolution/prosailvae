2f078d381f9416a3a9d68063f3f7b7b575ba2df7
Yoel Zerah <zerahy@cesbio.cnes.fr>
2023-01-18 12:06:08 +0100

    debug

--- prosailvae/encoders.py
+++ prosailvae/encoders.py
@@ -114,10 +114,10 @@ class ProsailNNEncoder(Encoder):
     
     def change_device(self, device):
         self.device=device
-        self.norm_mean.to(device)
-        self.norm_std.to(device)
-        self.net.to(device)
-        self.to(device)
+        self.norm_mean = self.norm_mean.to(device)
+        self.norm_std = self.norm_std.to(device)
+        self.net = self.net.to(device)
+        self = self.to(device)
 
     def encode(self, s2_refl, angles):
         

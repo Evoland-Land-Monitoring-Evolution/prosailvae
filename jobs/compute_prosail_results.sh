8edf653a043fb8507c04477dbd7023263440e128
Yoel Zerah <zerahy@cesbio.cnes.fr>
2023-01-18 15:53:33 +0100

    debug job

--- jobs/compute_prosail_results.sh
+++ jobs/compute_prosail_results.sh
@@ -1,11 +1,11 @@
-#PBS -N trainprosail
+#PBS -N resprosail
 #PBS -q qgpgpu
 #PBS -l select=1:ncpus=4:mem=60G:ngpus=1  
 #PBS -l walltime=12:00:00
 
 module load conda
 export LD_LIBRARY_PATH=/work/scratch/zerahy/dotconda/prosailvae/lib:$LD_LIBRAY_PATH
-conda activate /work/scratch/zerahy/dotconda/prosailvae
+conda activate /work/scratch/zerahy/dotconda/juanvae
 export SRCDIR=/home/uz/zerahy/projects/    
 export VAEDIR=${SRCDIR}/prosailvae/prosailvae/
 export RSR_DIR=/work/scratch/zerahy/prosailvae/data/

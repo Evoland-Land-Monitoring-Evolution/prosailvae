df9fb127e26e3ae382e07121e2dea62121986b5a
Yoel Zerah <zerahy@cesbio.cnes.fr>
2023-01-25 13:19:49 +0100

    plt fix

--- jobs/array_train_prosail.sh
+++ jobs/array_train_prosail.sh
@@ -2,7 +2,7 @@
 #PBS -q qgpgpu
 #PBS -l select=1:ncpus=4:mem=60G:ngpus=1  
 #PBS -l walltime=12:00:00
-#PBS -J 1-7:1
+#PBS -J 1-5:1
 
 module load conda
 export LD_LIBRARY_PATH=/work/scratch/zerahy/dotconda/prosailvae/lib:$LD_LIBRAY_PATH
@@ -14,7 +14,7 @@ export DATADIR=/work/scratch/zerahy/prosailvae/data/1e5_dataset_V2/
 export OUTDIR=/work/scratch/zerahy/prosailvae/results/${PBS_JOBID:0:8}_jobarray/${PBS_ARRAY_INDEX}/
 export TENSOR_DIR=/work/CESBIO/projects/MAESTRIA/prosail_validation/toyexample/torchfiles/
 export NUM_WORKERS=12
-export CONFIG_LIST=${VAEDIR}/config/list_configs.txt
+export CONFIG_LIST=${VAEDIR}/config/list_configs_regression.txt
 export CONFIG=$(sed -n ${PBS_ARRAY_INDEX}"p" $CONFIG_LIST)
 
 CUDA_LAUNCH_BLOCKING=1 python ${VAEDIR}/prosailvae/train.py -d ${DATADIR} -r $OUTDIR -rsr $RSR_DIR -t $TENSOR_DIR -c $CONFIG -a True

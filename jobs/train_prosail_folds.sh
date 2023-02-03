2daccd2944045bcbaede34c3475c3a459a2534f9
Yoel Zerah <zerahy@cesbio.cnes.fr>
2023-02-03 17:15:17 +0100

    job fix

--- jobs/train_prosail_folds.sh
+++ jobs/train_prosail_folds.sh
@@ -2,7 +2,7 @@
 #PBS -q qgpgpu
 #PBS -l select=1:ncpus=4:mem=60G:ngpus=1  
 #PBS -l walltime=02:00:00
-#PBS -J 1-3:1
+#PBS -J 0-2:1
 
 module load conda
 export LD_LIBRARY_PATH=/work/scratch/zerahy/dotconda/prosailvae/lib:$LD_LIBRAY_PATH
@@ -13,7 +13,7 @@ export K_FOLD=${PBS_ARRAY_INDEX}
 export NUMBER_OF_FOLDS=3
 
 
-python ${VAEDIR}/prosailvae/train.py -d ${FDATADIR} -r $FOUTDIR -rsr $FRSR_DIR -t $FTENSOR_DIR -c $FCONFIG -a True -n $NUMBER_OF_FOLDS -x $K_FOLD
+python ${VAEDIR}/prosailvae/train.py -d ${FDATADIR} -r $FOUTDIR -rsr $FRSR_DIR -t $FTENSOR_DIR -c $FCONFIG -a True -n $K_FOLD -x $NUMBER_OF_FOLDS 
 conda deactivate
 
 

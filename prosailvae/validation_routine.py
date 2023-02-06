e0598d26e359be87dbd20b4868f44f3fb4de2c04
Juan S Vinasco-Salinas <juan.vinasco@cesbio.cnes.fr>
2023-02-06 17:44:06 +0100

    WIP linting

--- prosailvae/validation_routine.py
+++ prosailvae/validation_routine.py
@@ -11,6 +11,12 @@ https://geopandas.org/en/stable/gallery/geopandas_rasterio_sample.html
 https://github.com/ArjanCodes/2022-abtest/blob/main/from_config/main.py
 """
 
+# TODO Read Vector Data
+# TODO Read Raster Data
+# TODO Get point values
+#
+
+
 import sys,os
 from typing import Any
 import json
@@ -24,11 +30,18 @@ import rasterio.plot as rio_plot
 
 @dataclass
 class Config:
+    """
+    Dataclass for hold the dataset
+    filenames
+    """
     raster: str
     vector: str
 
 
 def read_config_file() -> Config:
+    """
+    Func for retrieve the config file
+    """
     current_path = Path.cwd()
     parent_path = current_path.parent
     config_file = parent_path / "config" / "validation.json"
@@ -46,20 +59,22 @@ def main():
 
     # read geo data
     vector_data = gpd.read_file(vector_dataset_path)
-    raster_data = rio.open(raster_dataset_path)
+
+    with rio.open(raster_dataset_path) as raster:
+        raster_data = raster.read()
 
     # plot
-    fig, ax = plt.subplots()
+    # fig, ax = plt.subplots()
 
     # compute the extent of the raster
-    extent = [raster_data.bounds[0],
-              raster_data.bounds[1],
-              raster_data.bounds[2],
-              raster_data.bounds[3]]
+    # extent = [raster_data.bounds[0],
+    #           raster_data.bounds[1],
+    #           raster_data.bounds[2],
+    #           raster_data.bounds[3]]
 
-    ax = rio_plot.show(raster_data, extent=extent, ax=ax)
-    vector_data.plot(ax=ax)
-    plt.show()
+    # ax = rio_plot.show(raster_data, extent=extent, ax=ax)
+    # vector_data.plot(ax=ax)
+    # plt.show()
 
     # extract pixel values
     coord_list = [(x,y) for x,y

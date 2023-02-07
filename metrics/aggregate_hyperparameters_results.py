aa11f10435c523adcac38b15b8d91f4e31743351
Yoel Zerah <zerahy@cesbio.cnes.fr>
2023-02-07 08:54:04 +0100

    results gathering

--- metrics/aggregate_hyperparameters_results.py
+++ metrics/aggregate_hyperparameters_results.py
@@ -6,6 +6,10 @@ import prosailvae
 import torch
 from prosailvae.ProsailSimus import PROSAILVARS
 
+def get_immediate_subdirectories(a_dir):
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
+
 def get_prosailvae_results_gather_parser():
     """
     Creates a new argument parser.
@@ -28,8 +32,8 @@ def get_results_dirs_names():
                                                      os.pardir),"results/37099873_jobarray/")
     else:
         root_res_dir =  os.path.join(parser.root_results_dir, os.pardir)
-    with open(root_res_dir + "/results_directory_names.txt") as f:
-        res_dirs =  [line.rstrip() for line in f]
+    res_dirs = get_immediate_subdirectories(root_res_dir)
+
     return root_res_dir, res_dirs
 
 def plot_losses(val_losses, gathered_res_dir, model_names=None):
@@ -47,21 +51,22 @@ def plot_losses(val_losses, gathered_res_dir, model_names=None):
 
 
 def main():
+    n_folds = 5
     root_res_dir, res_dirs = get_results_dirs_names()
-    gathered_res_dir = root_res_dir + "/fold_results/"
+    gathered_res_dir = root_res_dir + "/agregated_results/"
     if not os.path.isdir(gathered_res_dir):
         os.makedirs(gathered_res_dir)
-    all_test_losses = torch.zeros((len(res_dirs),1))
+    all_test_losses = torch.zeros((len(res_dirs), n_folds, 1))
     alpha_pi = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 
                 0.6, 0.7, 0.8, 0.9, 0.95, 0.99]
-    all_aer_percentiles = torch.zeros((len(res_dirs), 5, len(PROSAILVARS)))
-    all_are_percentiles = torch.zeros((len(res_dirs), 5, len(PROSAILVARS)))
-    all_mae = torch.zeros((len(res_dirs), len(PROSAILVARS)))
-    all_maer = torch.zeros((len(res_dirs), len(PROSAILVARS)))
-    all_mpiw = torch.zeros((len(res_dirs), len(alpha_pi), len(PROSAILVARS)))
-    all_mpiwr = torch.zeros((len(res_dirs), len(alpha_pi), len(PROSAILVARS)))
-    all_picp = torch.zeros((len(res_dirs), len(alpha_pi), len(PROSAILVARS)))
-    all_lat_nll = torch.zeros((len(res_dirs), len(PROSAILVARS)))
+    all_aer_percentiles = torch.zeros((len(res_dirs), n_folds, 5, len(PROSAILVARS)))
+    all_are_percentiles = torch.zeros((len(res_dirs), n_folds, 5, len(PROSAILVARS)))
+    all_mae = torch.zeros((len(res_dirs), n_folds, len(PROSAILVARS)))
+    all_maer = torch.zeros((len(res_dirs), n_folds, len(PROSAILVARS)))
+    all_mpiw = torch.zeros((len(res_dirs), n_folds, len(alpha_pi), len(PROSAILVARS)))
+    all_mpiwr = torch.zeros((len(res_dirs), n_folds, len(alpha_pi), len(PROSAILVARS)))
+    all_picp = torch.zeros((len(res_dirs), n_folds, len(alpha_pi), len(PROSAILVARS)))
+    all_lat_nll = torch.zeros((len(res_dirs), n_folds, len(PROSAILVARS)))
     if os.path.isfile(root_res_dir + "/model_names.txt"):
         with open(root_res_dir + "/model_names.txt") as f:
             model_names =  [line.rstrip() for line in f]
@@ -70,14 +75,14 @@ def main():
     for i, dir_name in enumerate(res_dirs):
         test_loss = pd.read_csv(dir_name + "/loss/test_loss.csv")["loss_sum"].values[0]
         all_test_losses[i,0] = test_loss
-        all_picp[i,:,:] = torch.from_numpy(pd.read_csv(dir_name + "/metrics/picp.csv", index_col=[0]).drop(columns=["alpha"]).values)
-        all_mpiw[i,:,:] = torch.from_numpy(pd.read_csv(dir_name + "/metrics/mpiw.csv", index_col=[0]).drop(columns=["alpha"]).values)
-        all_mpiwr[i,:,:] = torch.from_numpy(pd.read_csv(dir_name + "/metrics/mpiwr.csv", index_col=[0]).drop(columns=["alpha"]).values)
-        all_mae[i,:] = torch.from_numpy(pd.read_csv(dir_name + "/metrics/mae.csv", index_col=[0]).values).squeeze()
-        all_maer[i,:] = torch.from_numpy(pd.read_csv(dir_name + "/metrics/maer.csv", index_col=[0]).values).squeeze()
-        all_lat_nll[i,:] = torch.load(dir_name + '/params_nll.pt').squeeze()
-        all_aer_percentiles[i,:,:] = torch.load(dir_name + '/metrics/aer_percentiles.pt')
-        all_are_percentiles[i,:,:] = torch.load(dir_name + '/metrics/are_percentiles.pt')     
+        all_picp[i,:,:,:] = torch.from_numpy(pd.read_csv(dir_name + "/metrics/picp.csv", index_col=[0]).values)
+        all_mpiw[i,:,:,:] = torch.from_numpy(pd.read_csv(dir_name + "/metrics/mpiw.csv", index_col=[0]).values)
+        all_mpiwr[i,:,:,:] = torch.from_numpy(pd.read_csv(dir_name + "/metrics/mpiwr.csv", index_col=[0]).values)
+        all_mae[i,:,:] = torch.from_numpy(pd.read_csv(dir_name + "/metrics/mae.csv", index_col=[0]).values).squeeze()
+        all_maer[i,:,:] = torch.from_numpy(pd.read_csv(dir_name + "/metrics/maer.csv", index_col=[0]).values).squeeze()
+        all_lat_nll[i,:,:] = torch.load(dir_name + '/params_nll.pt').squeeze()
+        all_aer_percentiles[i,:,:,:] = torch.load(dir_name + '/metrics/aer_percentiles.pt')
+        all_are_percentiles[i,:,:,:] = torch.load(dir_name + '/metrics/are_percentiles.pt')     
         pass
     pd.DataFrame(data=all_test_losses.reshape(1,-1).cpu().numpy(), columns=model_names).to_csv(gathered_res_dir+'/all_losses.csv')
     torch.save(all_picp, gathered_res_dir+'/picp.pt')

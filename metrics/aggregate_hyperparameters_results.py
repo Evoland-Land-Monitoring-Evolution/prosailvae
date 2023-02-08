7f195f442b0c9225f75a7c897699c4b8ad1bc88a
Yoel Zerah <zerahy@cesbio.cnes.fr>
2023-02-08 10:54:50 +0100

    result gathering fix

--- metrics/aggregate_hyperparameters_results.py
+++ metrics/aggregate_hyperparameters_results.py
@@ -55,10 +55,9 @@ def main():
     n_folds = 5
     root_res_dir, res_dirs = get_results_dirs_names()
     gathered_res_dir = root_res_dir + "/agregated_results/"
-    if not os.path.isdir(gathered_res_dir):
-        os.makedirs(gathered_res_dir)
-    else:
+    if os.path.isdir(gathered_res_dir):
         shutil.rmtree(gathered_res_dir)
+    os.makedirs(gathered_res_dir)
     all_test_losses = torch.zeros((len(res_dirs), n_folds, 1))
     alpha_pi = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 
                 0.6, 0.7, 0.8, 0.9, 0.95, 0.99]

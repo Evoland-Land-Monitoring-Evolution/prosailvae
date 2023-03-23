975381d6226327529f13b06dd1364232bd93725c
Yoel Zerah <zerahy@cesbio.cnes.fr>
2023-03-23 18:25:18 +0100

    debug and new plot ndvi vs lai

--- prosailvae/prosail_vae.py
+++ prosailvae/prosail_vae.py
@@ -46,7 +46,7 @@ def select_encoder(encoder_type, vae_params, device, refl_norm_mean, refl_norm_s
         first_layer_kernel = 3
         first_layer_size = 512
         crnn_group_sizes = [512,512]
-        crnn_group_depth = [1,2]
+        crnn_group_depth = [2,2]
         crnn_group_kernel_sizes = [3,1]
         crnn_group_n = [1,3]
         encoder = ProsailRCNNEncoder(s2refl_size=vae_params["input_size"], 

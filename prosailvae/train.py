9766d3095113d9fda76c6da94804ac6e9de2b571
Yoel Zerah <zerahy@cesbio.cnes.fr>
2023-04-06 08:20:26 +0200

    added lr_finder back for debug

--- prosailvae/train.py
+++ prosailvae/train.py
@@ -134,10 +134,10 @@ def training_loop(PROSAIL_VAE, optimizer, n_epoch, train_loader, valid_loader, l
     total_ram = get_total_RAM()
     old_lr = optimizer.param_groups[0]['lr']
     if exp_lr_decay > 0:
-        if lr_recompute is None:
-            lr_recompute = 20
-        lr_scheduler =  torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, patience=lr_recompute)
-        # lr_scheduler =  torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=exp_lr_decay)
+        # if lr_recompute is None:
+        #     lr_recompute = 20
+        # lr_scheduler =  torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, patience=lr_recompute)
+        lr_scheduler =  torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=exp_lr_decay)
     
     max_train_samples_per_epoch = 100
     max_valid_samples_per_epoch = None
@@ -148,9 +148,9 @@ def training_loop(PROSAIL_VAE, optimizer, n_epoch, train_loader, valid_loader, l
         for epoch in trange(n_epoch, desc='PROSAIL-VAE training', leave=True):
             t0=time.time()
             switch_loss(epoch, n_epoch, PROSAIL_VAE, swith_ratio=0.75)
-            # lr_scheduler, optimizer, old_lr = recompute_lr(lr_scheduler, PROSAIL_VAE, epoch, lr_recompute, exp_lr_decay, logger, 
-            #                                                optimizer, old_lr=old_lr, lrtrainloader=lrtrainloader, weiss_mode=weiss_mode,
-            #                                                n_samples=n_samples)
+            lr_scheduler, optimizer, old_lr = recompute_lr(lr_scheduler, PROSAIL_VAE, epoch, lr_recompute, exp_lr_decay, logger, 
+                                                           optimizer, old_lr=old_lr, lrtrainloader=lrtrainloader, weiss_mode=weiss_mode,
+                                                           n_samples=n_samples)
             info_df = pd.concat([info_df, pd.DataFrame({'epoch':epoch, "lr": optimizer.param_groups[0]['lr']}, index=[0])],ignore_index=True)
             try:
                 

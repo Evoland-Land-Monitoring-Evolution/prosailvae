0cdcdfec882c715f26362eb38e799e80607388e2
Yoel Zerah <zerahy@cesbio.cnes.fr>
2023-04-18 14:00:22 +0200

    job fix

--- jobs/snap_dist_analysis.sh
+++ jobs/snap_dist_analysis.sh
@@ -8,7 +8,7 @@ export LD_LIBRARY_PATH=/work/scratch/zerahy/dotconda/prosailvae/lib:$LD_LIBRAY_P
 conda activate /work/scratch/zerahy/dotconda/juanvae
 export SRCDIR=/home/uz/zerahy/projects/    
 export VAEDIR=${SRCDIR}/prosailvae/prosailvae/
-export DATADIR=/work/scratch/zerahy/prosailvae/data/1e5_dataset_V2/
+export DATADIR=/work/scratch/zerahy/prosailvae/data/snap_dataset/
 export OUTDIR=/work/scratch/zerahy/prosailvae/results/${PBS_JOBID:0:8}_snap_regressions/
 
 CUDA_LAUNCH_BLOCKING=1 python ${VAEDIR}/snap_regression/snap_regression.py -d ${DATADIR} -r $OUTDIR 

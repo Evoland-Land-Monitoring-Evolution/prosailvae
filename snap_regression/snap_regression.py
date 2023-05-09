e9c82be932181b17219773e0d5bdb9e612c1807c
Yoel Zerah <zerahy@cesbio.cnes.fr>
2023-05-09 14:00:22 +0200

    changed graphs

--- snap_regression/snap_regression.py
+++ snap_regression/snap_regression.py
@@ -358,7 +358,7 @@ def get_pixel_log_likelihood_with_weiss(s2_r, lai, n_components=128, max_iter=50
 
 def get_boxplot_symlog_width(positions:np.ndarray, threshold:float=0.01, linear_width:float = 0.1):
     symlog_width = np.zeros_like(positions)
-    symlog_width[np.where(positions <= threshold)] = linear_width
+    symlog_width[np.where(positions <= threshold)] = threshold * linear_width
     symlog_width[np.where(positions>threshold)] = 10**(np.log10(positions[positions>threshold])+ linear_width/2.)-10**(np.log10(positions[positions>threshold])-linear_width/2.)
 
     return symlog_width
@@ -379,8 +379,8 @@ def main():
         disable_tqdm=False
         # tg_mu = torch.tensor([0,1])
         # tg_sigma = torch.tensor([0.5,1])
-        tg_mu = torch.tensor([0,1])
-        tg_sigma = torch.tensor([0.5,1])
+        tg_mu = torch.tensor([0,2])
+        tg_sigma = torch.tensor([0.5,3])
         parser = get_parser().parse_args(args)
         s2_tensor_image_path = "/home/yoel/Documents/Dev/PROSAIL-VAE/prosailvae/data/torch_files/T31TCJ/after_SENTINEL2B_20171127-105827-648_L2A_T31TCJ_C_V2-2_roi_0.pth"  
         ver="3B"
@@ -674,6 +674,7 @@ def main():
                 ax.set_xlabel("KL distance between distribution of lai in training and evaluation dataset")
                 ax.set_ylabel(metrics_names[k])
                 ax.set_xscale('symlog', linthresh=linthresh)
+                ax.set_xlim(xmin=-1e-3)
                 fig.savefig(kl_res_dir + f"/boxplot_{eval_data_name[j]}_{metrics_names[k]}_vs_kl.png")
 
                 fig, ax = plt.subplots(1, dpi=150, tight_layout=True)
@@ -732,6 +733,7 @@ def main():
                 ax.set_xlabel("sampling distribution mu")
                 ax.set_ylabel(metrics_names[k])
                 ax.set_xscale('symlog',linthresh=linthresh)
+                ax.set_xlim(xmin=-1e-3)
                 fig.savefig(mu_res_dir + f"/boxplot_{eval_data_name[j]}_{metrics_names[k]}_vs_mu.png")
 
                 fig, ax = plt.subplots(1, dpi=150, tight_layout=True)
@@ -785,6 +787,7 @@ def main():
                 ax.set_xlabel("sampling distribution sigma")
                 ax.set_ylabel(metrics_names[k])
                 ax.set_xscale('symlog', linthresh=linthresh)
+                ax.set_xlim(xmin=-1e-3)
                 fig.savefig(sigma_res_dir + f"/boxplot_{eval_data_name[j]}_{metrics_names[k]}_vs_sigma.png")
 
                 fig, ax = plt.subplots(1, dpi=150, tight_layout=True)

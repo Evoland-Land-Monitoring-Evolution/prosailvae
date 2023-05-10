90d0ceedccd02c843853a1596c0d0c558a989fa5
Yoel Zerah <zerahy@cesbio.cnes.fr>
2023-05-10 15:06:30 +0200

    fix

--- metrics/results.py
+++ metrics/results.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     from snap_regression.snap_regression import SnapNN
 else:
     from metrics.metrics import get_metrics, save_metrics, get_juan_validation_metrics, get_weiss_validation_metrics
-    from prosail_plots import (plot_metrics, plot_rec_and_latent, loss_curve, plot_param_dist, plot_pred_vs_tgt, 
+    from metrics.prosail_plots import (plot_metrics, plot_rec_and_latent, loss_curve, plot_param_dist, plot_pred_vs_tgt, 
                                        plot_refl_dist, pair_plot, plot_rec_error_vs_angles, plot_lat_hist2D, plot_rec_hist2D, 
                                        plot_metric_boxplot, plot_patch_pairs, plot_lai_preds, plot_single_lat_hist_2D,
                                        all_loss_curve, plot_patches, plot_lai_vs_ndvi, PROSAIL_2D_res_plots, PROSAIL_2D_aggregated_results)

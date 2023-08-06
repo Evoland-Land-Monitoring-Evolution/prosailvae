f6ccc504b575af64fb6f391ddfd16bbc9d07f3a7
Yoel Zerah <zerahy@cesbio.cnes.fr>
2023-08-07 00:32:22 +0200

    fix

--- prosailvae/prosail_vae.py
+++ prosailvae/prosail_vae.py
@@ -89,7 +89,6 @@ def load_params(config_dir, config_file, parser=None):
         params["beta_cyclical"] = 0
     if "lat_loss_type" not in params.keys():
         params["lat_loss_type"] = ""
-    lrs_threshold
     if "lrs_threshold" not in params.keys():
         params["lrs_threshold"] = 5e-3
     return params

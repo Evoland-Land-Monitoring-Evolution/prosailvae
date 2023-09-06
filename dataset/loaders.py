refactored results computation and corrected cyclical data-set

dataset/loaders.py
@@ -445,7 +445,7 @@ def get_train_valid_test_loader_from_patches(path_to_patches_dir, bands=torch.ar
                                             concat=concat)
     valid_loader = get_loader_from_patches(path_to_valid_patches, bands = bands,
                                             batch_size=batch_size, num_workers=num_workers,
-                                            max_samples=max_valid_samples, concat=concat)
+                                            max_samples=max_valid_samples, concat=concat, shuffle=False)
     test_loader = get_loader_from_patches(path_to_test_patches, bands = bands,
                                             batch_size=batch_size, num_workers=num_workers, concat=concat,
                                             shuffle=False)

dataset/project_s2_dataset.py
@@ -56,7 +56,7 @@ def get_model_and_dataloader(parser):
     info_test_data = np.load(os.path.join(parser.data_dir, "test_info.npy"))
     return model_dict, train_loader, test_loader, valid_loader, info_test_data
    
-def project_loader_patches(model, loader, mode="lat_mode", loader_output=False, batch_size=1, max_batches=50):
+def project_loader_patches(model, loader, mode="lat_mode", loader_output=False, batch_size=1, max_batches=None):
     inferred_BV = []
     projected_images = []
     angles = []
@@ -115,7 +115,7 @@ def project_loader_pixellic(model, loader, mode = 'lat_mode', loader_output=Fals
         return (prosail_s2_sim_refl.detach().cpu(), 
                 prosail_sim_vars.detach().cpu())
 
-def save_common_cyclical_dataset(model_dict, loader, output_dir=""):
+def save_common_cyclical_dataset(model_dict, loader, output_dir="", max_batches=None):
     all_s2_r = []
     all_s2_a = []
     all_sim = []
@@ -126,7 +126,7 @@ def save_common_cyclical_dataset(model_dict, loader, output_dir=""):
     for _, model_info in model_dict.items():
         model = model_info["model"]
         delta_hw = max_hw - model.encoder.nb_enc_cropped_hw
-        s2_r, s2_a, sim = project_loader_patches(model, loader, mode="lat_mode", loader_output=False, batch_size=1)
+        s2_r, s2_a, sim = project_loader_patches(model, loader, mode="lat_mode", loader_output=False, batch_size=1, max_batches=max_batches)
         if delta_hw > 0:
             s2_r = crop_s2_input(s2_r, delta_hw)
             s2_a = crop_s2_input(s2_a, delta_hw)

validation/validation.py
@@ -150,22 +150,22 @@ def get_belsar_x_frm4veg_lai_results(belsar_results, barrax_results, barrax_2021
         else:
             bands_rec_err[band] = np.zeros_like(ref_lai)
 
-    results = pd.DataFrame(data={'LAI':ref_lai,
-                                'LAI std':np.concatenate(ref_lai_std_list),
-                                'Predicted LAI': np.concatenate(pred_lai_list),
-                                'Predicted LAI std':np.concatenate(pred_lai_std_list),
-                                "Site": np.array(site_list),
-                                "Land cover": np.concatenate(land_cover_list),
-                                "Reconstruction error": rec_err,
-                                "Time delta": np.concatenate(date_list),
-                                "Campaign": np.array(campaign_list),
+    results = pd.DataFrame(data={f'{frm4veg_lai}':ref_lai,
+                                 f'{frm4veg_lai} std':np.concatenate(ref_lai_std_list),
+                                 f'Predicted {frm4veg_lai}': np.concatenate(pred_lai_list),
+                                 f'Predicted {frm4veg_lai} std':np.concatenate(pred_lai_std_list),
+                                 "Site": np.array(site_list),
+                                 "Land cover": np.concatenate(land_cover_list),
+                                 "Reconstruction error": rec_err,
+                                 "Time delta": np.concatenate(date_list),
+                                 "Campaign": np.array(campaign_list),
                                 })
     for band in np.array(BANDS)[bands_idx.cpu()].tolist():
         results[f"{band} error"] = bands_rec_err[band]
     return results
 
 def get_frm4veg_ccc_results(barrax_results, barrax_2021_results, wytham_results,
-                                     frm4veg_ccc="ccc", get_reconstruction_error=False, bands_idx=torch.arange(10)):
+                            frm4veg_ccc="ccc", get_reconstruction_error=False, bands_idx=torch.arange(10)):
 
 
     date_list = [barrax_results[f'{frm4veg_ccc}_date'].reshape(-1),
@@ -215,10 +215,10 @@ def get_frm4veg_ccc_results(barrax_results, barrax_2021_results, wytham_results,
                                                     wytham_results[f'{frm4veg_ccc}_{band}_rec_err'].reshape(-1)])
         else:
             bands_rec_err[band] = np.zeros_like(ref_ccc)
-    results = pd.DataFrame(data={'CCC':ref_ccc,
-                                'CCC std':np.concatenate(ref_ccc_std_list),
-                                'Predicted CCC': np.concatenate(pred_ccc_list),
-                                'Predicted CCC std':np.concatenate(pred_ccc_std_list),
+    results = pd.DataFrame(data={f'{frm4veg_ccc}':ref_ccc,
+                                f'{frm4veg_ccc} std':np.concatenate(ref_ccc_std_list),
+                                f'Predicted {frm4veg_ccc}': np.concatenate(pred_ccc_list),
+                                f'Predicted {frm4veg_ccc} std':np.concatenate(pred_ccc_std_list),
                                 "Site": np.array(site_list),
                                 "Land cover": np.concatenate(land_cover_list),
                                 "Reconstruction error": rec_err,
@@ -230,7 +230,7 @@ def get_frm4veg_ccc_results(barrax_results, barrax_2021_results, wytham_results,
     return results
 
 def get_validation_global_metrics(df_results, decompose_along_columns=["Site", "Land cover"], 
-                                  n_sigma=3, variable="LAI"):
+                                  n_sigma=3, variable="lai"):
     global_rmse_dict = {}
     global_picp_dict = {}
     global_mpiw_dict = {}
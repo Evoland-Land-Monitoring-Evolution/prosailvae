654ab9a2b53efef19ef1a89dbfed2a7cb23dffd8
Yoel Zerah <zerahy@cesbio.cnes.fr>
2023-09-26 19:25:57 +0200

    fix

--- metrics/results.py
+++ metrics/results.py
@@ -266,7 +266,7 @@ def save_validation_results(model, res_dir,
         tikzplotlib.save(os.path.join(hspot_dir, f"{model_name}_belSAR_LAI_vs_hspot_Maize.tex"))
 
         for variable in ["lai", "ccc"]:
-            fig, axs = plt.subplots(7, 14, dpi=150)
+            fig, axs = plt.subplots(1,2, figsize=(7, 14), dpi=150)
             _, _ = regression_plot_2hues(df_results_snap[variable], x=f"{variable}", y=f"Predicted {variable}", 
                                          hue="Land cover", hue2="Campaign", display_text=False,
                                              error_x=f"{variable} std", 

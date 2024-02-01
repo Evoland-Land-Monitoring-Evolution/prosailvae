b5ea695570f9c75cc107a285960b1e0ab3fea1a0
Jordi Inglada <jordi.inglada@cesbio.eu>
2024-02-01 17:11:49 +0100

    TEST: use small data for test

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,22 +1,35 @@
 from pathlib import Path
 
 import pytest
+import torch
 from pytorch_lightning import Trainer
 
 from prosailvae.datamodules.data_module import DataModuleConfig, ProsailVAEDataModule
 
-from .test_lightning_module import test_lightning_instanciate
+from .test_dataloader import PATCHES_DIR
+from .test_lightning_module import instanciate
+
+TMP_DIR = Path("/tmp")
+
+
+def prepare_data():
+    fname = "train_patches.pth"
+    x = torch.load(PATCHES_DIR / fname)
+    y = x[:10, :]
+    torch.save(y, TMP_DIR / fname)
+    torch.save(y, TMP_DIR / "valid_patches.pth")
+    torch.save(y, TMP_DIR / "test_patches.pth")
 
 
 @pytest.mark.slow
 def test_train_pipeline():
+    prepare_data()
     bands = 10
-    lit_mod = test_lightning_instanciate(bands=bands)
-
-    cfg = DataModuleConfig(Path("/tmp"), list(range(bands)))
+    lit_mod = instanciate(bands=bands)
+    cfg = DataModuleConfig(TMP_DIR, list(range(bands)))
     data_mod = ProsailVAEDataModule(cfg)
     trainer = Trainer(num_sanity_val_steps=1, max_epochs=1)
     trainer.fit(model=lit_mod, datamodule=data_mod)
     optimized_metric = "train/loss"
     score = trainer.callback_metrics.get(optimized_metric)
-    return score
+    assert score is not None

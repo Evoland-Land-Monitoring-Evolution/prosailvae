6b080c49216be94af649ad9bea5cc590a24614c8
Jordi Inglada <jordi.inglada@cesbio.eu>
2024-02-29 16:52:20 +0100

    TEST: add tests for torch model and refactor fixtures

--- tests/test_lightning_module.py
+++ tests/test_lightning_module.py
@@ -1,34 +1,14 @@
-from pathlib import Path
-
-import torch
-
-from prosailvae.encoders import EncoderConfig
-from prosailvae.loss import LossConfig
 from prosailvae.models.lightning_module import ProsailVAELightningModule
-from prosailvae.prosail_vae import ProsailVAEConfig
-from prosailvae.utils.utils import load_standardize_coeffs
 
-SRC_DIR = Path(__file__).parent.parent
-PATCHES_DIR = Path("/usr/local/stok/DATA/MMDC/ProsailVAE/PROSAILVAE/s2_patch_dataset/")
-RSR_DIR = SRC_DIR / "data"
-N_PROSAIL_VARS = 11
+from .test_simvae import generate_config
 
 
 def instanciate(bands: int = 10, lat_idx: int = 6) -> ProsailVAELightningModule:
-    io_coeffs = load_standardize_coeffs(PATCHES_DIR)
-    n_idx = io_coeffs.idx.loc.size(0) if io_coeffs.idx.loc is not None else 0
-    enc_conf = EncoderConfig(
-        encoder_type="rnn",
-        input_size=bands + 3 + n_idx,
-        io_coeffs=io_coeffs,
-        output_size=N_PROSAIL_VARS,
-    )
-    loss_conf = LossConfig(lat_idx=torch.tensor(lat_idx).int())
-    pv_conf = ProsailVAEConfig(enc_conf, loss_conf, RSR_DIR, "/tmp", "/tmp")
+    pv_conf = generate_config()
     mod = ProsailVAELightningModule(pv_conf)
     return mod
 
 
 def test_lightning_instanciate(bands: int = 10, lat_idx: int = 6) -> None:
-    module = instanciate()
+    module = instanciate(bands, lat_idx)
     assert module is not None
---
+++ tests/test_simvae.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+import torch
+
+from prosailvae.encoders import EncoderConfig
+from prosailvae.loss import LossConfig
+from prosailvae.prosail_vae import ProsailVAEConfig, get_prosail_vae
+from prosailvae.utils.utils import load_standardize_coeffs
+
+SRC_DIR = Path(__file__).parent.parent
+PATCHES_DIR = Path("/usr/local/stok/DATA/MMDC/ProsailVAE/PROSAILVAE/s2_patch_dataset/")
+RSR_DIR = SRC_DIR / "data"
+N_PROSAIL_VARS = 11
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def generate_config(bands: int = 10, lat_idx: int = 6):
+    io_coeffs = load_standardize_coeffs(PATCHES_DIR)
+    n_idx = io_coeffs.idx.loc.size(0) if io_coeffs.idx.loc is not None else 0
+    enc_conf = EncoderConfig(
+        encoder_type="rnn",
+        input_size=bands + 3 + n_idx,
+        io_coeffs=io_coeffs,
+        output_size=N_PROSAIL_VARS,
+    )
+    loss_conf = LossConfig(lat_idx=torch.tensor(lat_idx).int())
+    pv_conf = ProsailVAEConfig(enc_conf, loss_conf, RSR_DIR, "/tmp", "/tmp")
+    return pv_conf
+
+
+def test_instantiate():
+    config = generate_config()
+    model = get_prosail_vae(config, DEVICE)
+    assert model is not None
+
+
+def test_forward():
+    batch_size = 2
+    patch_size = 4
+    config = generate_config()
+    model = get_prosail_vae(config, DEVICE)
+    data = torch.rand(batch_size, 10, patch_size, patch_size)
+    angles = torch.rand(batch_size, 3, patch_size, patch_size)
+    dist_params, z, phi, rec = model.forward(data, angles=angles, n_samples=3)
+    rec.sum().backward()

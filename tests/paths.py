6f762b7806cf03ddb6a7cbb3e4e8ef3636013ea9
Jordi Inglada <jordi.inglada@cesbio.eu>
2024-03-13 12:29:30 +0100

    BUG: typo in path

--- tests/paths.py
+++ tests/paths.py
@@ -4,5 +4,5 @@ ROOT_DIR = "/work/CESBIO/projects/DeepChange/PROSAILVAE"
 if Path("/usr/local/stok/DATA/MMDC").exists():
     ROOT_DIR = "/usr/local/stok/DATA/MMDC/ProsailVAE/PROSAILVAE"
 elif Path("/gpfsscratch/rech/adz/uzh16pa/ProsailVAE").exists():
-    ROOT_DIR = "/gpfsscratch/rech/adz/uzh16pam/ProsailVAE/PROSAILVAE"
+    ROOT_DIR = "/gpfsscratch/rech/adz/uzh16pa/ProsailVAE/PROSAILVAE"
 PATCHES_DIR = Path(f"{ROOT_DIR}/s2_patch_dataset/")

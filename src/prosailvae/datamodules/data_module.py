191ca0dcf942f714cce4ab0c0234d8cd0aee9740
Jordi Inglada <jordi.inglada@cesbio.eu>
2024-03-20 15:24:04 +0100

    REFAC: options to limit the nb of total samples

--- src/prosailvae/datamodules/data_module.py
+++ src/prosailvae/datamodules/data_module.py
@@ -1,6 +1,7 @@
 """
 ProsailVAE Lightning Data Module
 """
+
 from dataclasses import dataclass
 from pathlib import Path
 from typing import cast
@@ -20,6 +21,7 @@ class DataModuleConfig:
     bands: list[int]
     batch_size: int = 1
     num_workers: int = 0
+    max_training_samples: int | None = None
     max_valid_samples: int = 50
     concat: bool = False
 
@@ -37,21 +39,26 @@ class ProsailVAEDataModule(LightningDataModule):
         self.bands = torch.as_tensor(config.bands)
         self.batch_size = config.batch_size
         self.num_workers = config.num_workers
+        self.max_training_samples = config.max_training_samples
         self.max_valid_samples = config.max_valid_samples
         self.concat = config.concat
 
     def train_dataloader(self) -> DataLoader:
-        return self.instanciate_dataloader("train_patches.pth", shuffle=True)
+        return self.instanciate_dataloader(
+            "train_patches.pth", self.max_training_samples, shuffle=True
+        )
 
     def val_dataloader(self) -> DataLoader:
         """Instanciate and return a validation data loader"""
-        return self.instanciate_dataloader("valid_patches.pth")
+        return self.instanciate_dataloader("valid_patches.pth", self.max_valid_samples)
 
     def test_dataloader(self) -> DataLoader:
         """Instanciate and return a testing data loader"""
         return self.instanciate_dataloader("test_patches.pth")
 
-    def instanciate_dataloader(self, fname: str, shuffle: bool = False) -> DataLoader:
+    def instanciate_dataloader(
+        self, fname: str, max_samples: int | None = None, shuffle: bool = False
+    ) -> DataLoader:
         """Instanciate and return a data loader using patches from the given file"""
         return cast(
             DataLoader,
@@ -61,6 +68,7 @@ class ProsailVAEDataModule(LightningDataModule):
                 batch_size=self.batch_size,
                 num_workers=self.num_workers,
                 concat=self.concat,
+                max_samples=max_samples,
                 shuffle=shuffle,
             ),
         )

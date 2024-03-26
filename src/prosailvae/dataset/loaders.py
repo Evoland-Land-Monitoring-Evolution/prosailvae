ee37dfd5481ebe7a9659bd0497abc064a2004f01
Jordi Inglada <jordi.inglada@cesbio.eu>
2024-03-26 16:11:13 +0100

    REFAC: ATTENTION removed unused functions

--- src/prosailvae/dataset/loaders.py
+++ src/prosailvae/dataset/loaders.py
@@ -5,8 +5,8 @@ Created on Tue Oct 25 13:39:40 2022
 @author: yoel
 """
 import argparse
+import logging
 import os
-import socket
 from pathlib import Path
 
 import numpy as np
@@ -17,41 +17,26 @@ from mmdc_singledate.datamodules.components.datamodule_utils import (
 )
 from mmdc_singledate.datamodules.mmdc_datamodule import (
     IterableMMDCDataset,
-    destructure_batch,
     worker_init_fn,
 )
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, TensorDataset
-from torchutils.patches import patchify
 
 import prosailvae
 
-# (MMDCDataStats, OneSetMMDCDataclass, average_stats, compute_stats,)
+# Configure logging
+NUMERIC_LEVEL = getattr(logging, "INFO", None)
+logging.basicConfig(
+    level=NUMERIC_LEVEL, format="%(asctime)-15s %(levelname)s: %(message)s"
+)
+
+logger = logging.getLogger(__name__)
 
 
-def split_train_valid_by_fid(labels, ts_ids, valid_size=0.1, seed=42):
-    """split database in train and valid subset according to a column name
-
-    Parameters
-    ----------
-
-    database_file
-        sqlite database file
-    labels_column
-        labels column
-    origin_fid_column
-        column containing original fids
-    test_size
-        percentage of samples use to valid the model [0;1]
-    seed
-        random seed to split the database in train/validation sample-set
-
-    Note
-    ----
-
-    Return a tuple of two list, the first list is dedicated to learn model
-    and the second to validated it
-    """
+def split_train_valid_by_fid(
+    labels: torch.Tensor, ts_ids: torch.Tensor, valid_size: float = 0.1, seed: int = 42
+) -> tuple[list[str], list[str]]:
+    """split database in train and valid subset according to a column name"""
 
     label_to_fids = dict(
         pd.DataFrame(
@@ -77,7 +62,12 @@ def split_train_valid_by_fid(labels, ts_ids, valid_size=0.1, seed=42):
     return fids_train, fids_valid
 
 
-def save_ids_for_k_fold(k=2, test_ratio=0.01, file_prefix="s2_", data_dir=None):
+def save_ids_for_k_fold(
+    k: int = 2,
+    test_ratio: float = 0.01,
+    file_prefix: str = "s2_",
+    data_dir: str | None = None,
+) -> None:
     if data_dir is None:
         data_dir = os.path.join(
             os.path.join(os.path.dirname(prosailvae.__file__), os.pardir), "data/"
@@ -115,71 +105,6 @@ def save_ids_for_k_fold(k=2, test_ratio=0.01, file_prefix="s2_", data_dir=None):
         )
     else:
         raise NotImplementedError
-
-
-def get_s2loader(
-    valid_ratio=None,
-    ts_ids=None,
-    batch_size=1024,
-    num_workers=0,
-    file_prefix="s2_",
-    data_dir=None,
-):
-    if data_dir is None:
-        data_dir = os.path.join(
-            os.path.join(os.path.dirname(prosailvae.__file__), os.pardir), "data/"
-        )
-    ndvi = torch.load(data_dir + f"/{file_prefix}ndvi_dataset.pt")
-    labels = torch.load(data_dir + f"/{file_prefix}labels.pt")
-    if ts_ids is None:
-        sub_ndvi = ndvi
-        sub_labels = labels
-    else:
-        sub_ndvi = ndvi[ts_ids, :]
-        sub_labels = labels[ts_ids, :]
-
-    if valid_ratio is None:
-        dataset = TensorDataset(sub_ndvi.float(), sub_labels.float())
-        loader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers)
-        return loader
-    else:
-        ids_train, ids_valid = split_train_valid_by_fid(
-            sub_labels, ts_ids, valid_size=0.1
-        )
-        sub_ndvi_train = ndvi[ids_train, :]
-        sub_labels_train = labels[ids_train, :]
-        sub_ndvi_valid = ndvi[ids_valid, :]
-        sub_labels_valid = labels[ids_valid, :]
-        train_dataset = TensorDataset(sub_ndvi_train.float(), sub_labels_train.float())
-        train_loader = DataLoader(
-            train_dataset, batch_size=batch_size, num_workers=num_workers
-        )
-        valid_dataset = TensorDataset(sub_ndvi_valid.float(), sub_labels_valid.float())
-        valid_loader = DataLoader(
-            valid_dataset, batch_size=batch_size, num_workers=num_workers
-        )
-        return train_loader, valid_loader
-
-
-def load_train_valid_ids(k=None, n=None, file_prefix="s2_", data_dir=None):
-    if data_dir is None:
-        data_dir = os.path.join(
-            os.path.join(os.path.dirname(prosailvae.__file__), os.pardir), "data/"
-        )
-    if k is None or n is None:
-        train_valid_ids = torch.load(data_dir + f"/{file_prefix}train_valid_ids.pt")
-    else:
-        train_valid_ids = torch.load(
-            data_dir + f"/{file_prefix}train_valid_ids_{k}_{n}.pt"
-        )
-    return train_valid_ids
-
-
-def load_test_ids(file_prefix="s2_", data_dir=None):
-    data_dir = os.path.join(
-        os.path.join(os.path.dirname(prosailvae.__file__), os.pardir), "data/"
-    )
-    return torch.load(data_dir + f"/{file_prefix}test_ids.pt")
 
 
 def lr_finder_loader(
@@ -225,19 +150,6 @@ def lr_finder_loader(
         loader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers)
 
     else:
-        # _,loader,_, = get_mmdc_loaders(tensors_dir=tensors_dir,
-        #                                 batch_size=1,
-        #                                 batch_par_epoch=100,
-        #                                 max_open_files=4,
-        #                                 num_workers=1,
-        #                                 pin_memory=False)
-        path_to_image = (
-            tensors_dir
-            + "/after_SENTINEL2B_20171127-105827-648_L2A_T31TCJ_C_V2-2_roi_0.pth"
-        )
-        # _, loader, _ = get_loaders_from_image(path_to_image, patch_size=32, train_ratio=0.8, valid_ratio=0.1,
-        #                                        bands = torch.tensor([0,1,2,4,5,6,3,7,8,9]), n_patches_max = 100,
-        #                                         batch_size=1, num_workers=0, concat=True)
         loader, _, _ = get_train_valid_test_loader_from_patches(
             data_dir, bands=torch.arange(10), batch_size=1, num_workers=0, concat=True
         )
@@ -359,7 +271,8 @@ def get_norm_coefs(data_dir, file_prefix=""):
 
 
 def convert_angles(angles):
-    # TODO: convert 6 S2 "angles" into sun zenith, S2 zenith and Sun/S2 relative Azimuth (degrees)
+    # TODO: convert 6 S2 "angles" into sun zenith, S2 zenith and Sun/S2
+    # relative Azimuth (degrees)
     c_sun_zen = angles[:, 0].unsqueeze(1)
     c_sun_azi = angles[:, 1].unsqueeze(1)
     s_sun_azi = angles[:, 2].unsqueeze(1)
@@ -373,8 +286,6 @@ def convert_angles(angles):
     sun_zen = torch.rad2deg(torch.arccos(c_sun_zen))
     obs_zen = torch.rad2deg(torch.arccos(c_obs_zen))
     rel_azi = torch.rad2deg(torch.atan2(s_rel_azi, c_rel_azi)) % 360
-    sun_azi = torch.rad2deg(torch.atan2(s_sun_azi, c_sun_azi)) % 360
-    obs_azi = torch.rad2deg(torch.atan2(s_obs_azi, c_obs_azi)) % 360
     return torch.concat((sun_zen, obs_zen, rel_azi), axis=1)
 
 
@@ -393,23 +304,6 @@ def flatten_patch(s2_refl, angles):
         .reshape(batch_size * patch_size_x * patch_size_y, 6)
     )
     return s2_refl, angles
-
-
-def patchify_2D_tensor(tensor_2D, data_size=10, patch_size=32):
-    mixed_batch_size = tensor_2D.size(0)
-    batch_size = mixed_batch_size // patch_size**2
-    patchified_tensor = (
-        tensor_2D.reshape(batch_size, patch_size, patch_size, data_size)
-        .transpose(2, 3)
-        .transpose(1, 2)
-    )
-    return patchified_tensor
-
-
-def get_flattened_patch(batch, device="cpu"):
-    (s2_r, s2_a, _, _, _, _, _) = destructure_batch(batch)
-    s2_r, s2_a = flatten_patch(s2_r.to(device), s2_a.to(device))
-    return s2_r, s2_a
 
 
 def get_mmdc_loaders(
@@ -479,102 +373,16 @@ def get_mmdc_loaders(
     return train_dataloader, val_dataloader, test_dataloader
 
 
-def get_loaders_from_image(
-    path_to_image,
-    patch_size=32,
-    train_ratio=0.8,
-    valid_ratio=0.1,
-    bands=torch.tensor([0, 1, 2, 4, 5, 6, 3, 7, 8, 9]),
-    n_patches_max=100,
-    batch_size=1,
-    num_workers=0,
-    concat=False,
-    max_im_size=1024,
-    seed=2147483647,
-):
-    if socket.gethostname() == "CELL200973":
-        patch_size = 16
-    assert train_ratio + valid_ratio <= 1
-    image_tensor = torch.load(path_to_image)
-    max_im_size = min(max_im_size, image_tensor.size(1), image_tensor.size(2))
-    image_tensor = image_tensor[:, :max_im_size, :max_im_size]
-    angles = torch.zeros(3, image_tensor.size(1), image_tensor.size(2))
-    angles[0, ...] = image_tensor[11, ...]
-    angles[1, ...] = image_tensor[13, ...]
-    angles[2, ...] = image_tensor[12, ...] - image_tensor[14, ...]
-    s2_r = image_tensor[bands, ...]
-    s2_r_patches = patchify(s2_r, patch_size=patch_size, margin=0).reshape(
-        -1, len(bands), patch_size, patch_size
-    )
-    s2_a_patches = patchify(angles, patch_size=patch_size, margin=0).reshape(
-        -1, 3, patch_size, patch_size
-    )
-    n_patches = min(s2_a_patches.size(0), n_patches_max)
-    g_cpu = torch.Generator()
-    g_cpu.manual_seed(seed)
-    perms = torch.randperm(s2_r_patches.size(0), generator=g_cpu)
-    train_idx = perms[torch.arange(int(train_ratio * n_patches))]
-    valid_idx = perms[
-        torch.arange(
-            int(train_ratio * n_patches), int((valid_ratio + train_ratio) * n_patches)
-        )
-    ]
-    test_idx = perms[
-        torch.arange(int((valid_ratio + train_ratio) * n_patches), n_patches)
-    ]
-    if concat:
-        train_dataset = TensorDataset(
-            torch.cat(
-                (s2_r_patches[train_idx, ...], s2_a_patches[train_idx, ...]), axis=1
-            )
-        )
-        valid_dataset = TensorDataset(
-            torch.cat(
-                (s2_r_patches[valid_idx, ...], s2_a_patches[valid_idx, ...]), axis=1
-            )
-        )
-        test_dataset = TensorDataset(
-            torch.cat(
-                (s2_r_patches[test_idx, ...], s2_a_patches[test_idx, ...]), axis=1
-            )
-        )
-    else:
-        train_dataset = TensorDataset(
-            s2_r_patches[train_idx, ...], s2_a_patches[train_idx, ...]
-        )
-        valid_dataset = TensorDataset(
-            s2_r_patches[valid_idx, ...], s2_a_patches[valid_idx, ...]
-        )
-        test_dataset = TensorDataset(
-            s2_r_patches[test_idx, ...], s2_a_patches[test_idx, ...]
-        )
-    train_loader = DataLoader(
-        dataset=train_dataset,
-        batch_size=batch_size,
-        num_workers=num_workers,
-        shuffle=True,
-    )
-    valid_loader = DataLoader(
-        dataset=valid_dataset,
-        batch_size=batch_size,
-        num_workers=num_workers,
-        shuffle=True,
-    )
-    test_loader = DataLoader(
-        dataset=test_dataset, batch_size=batch_size, num_workers=num_workers
-    )
-    return train_loader, valid_loader, test_loader
-
-
 def get_train_valid_test_loader_from_patches(
     path_to_patches_dir,
-    bands=torch.arange(10),
+    bands=None,
     batch_size=1,
     num_workers=0,
     max_valid_samples=50,
     concat=False,
 ):
-    # bands = torch.tensor([0,1,2,4,5,6,3,7,8,9])
+    if bands is None:
+        bands = torch.arange(10)
     path_to_train_patches = os.path.join(path_to_patches_dir, "train_patches.pth")
     path_to_valid_patches = os.path.join(path_to_patches_dir, "valid_patches.pth")
     path_to_test_patches = os.path.join(path_to_patches_dir, "test_patches.pth")
@@ -607,7 +415,7 @@ def get_train_valid_test_loader_from_patches(
 
 def get_loader_from_patches(
     path_to_patches,
-    bands=torch.tensor([0, 1, 2, 4, 5, 6, 3, 7, 8, 9]),
+    bands=None,
     batch_size=1,
     num_workers=0,
     concat=False,
@@ -615,6 +423,8 @@ def get_loader_from_patches(
     shuffle=True,
     plot_distribution=False,
 ):
+    if bands is None:
+        bands = torch.tensor([0, 1, 2, 4, 5, 6, 3, 7, 8, 9])
     patches = torch.load(path_to_patches)
     s2_a_patches = torch.zeros(patches.size(0), 3, patches.size(2), patches.size(3))
     s2_a_patches[:, 0, ...] = patches[:, 11, ...]  # sun zenith
@@ -623,15 +433,17 @@ def get_loader_from_patches(
         patches[:, 12, ...] - patches[:, 14, ...]
     )  # relative azimuth : sun azimuth - joint azimuth
     s2_r_patches = patches[:, bands, ...]
-    if plot_distribution:
-        pass
-    print(
-        f"sample mean of bands values in loader: {patches[0,:10,:,:].reshape(10,-1).mean(1).cpu()}"
+
+    logger.info(
+        "sample mean of bands values in loader: "
+        f"{patches[0,:10,:,:].reshape(10,-1).mean(1).cpu()}"
     )
+    logger.info(f"Loading from {path_to_patches}")
     if max_samples is not None:
         max_samples = min(max_samples, s2_r_patches.size(0))
         s2_r_patches = s2_r_patches[:max_samples, ...]
         s2_a_patches = s2_a_patches[:max_samples, ...]
+        logger.info(f"Limit to {max_samples} samples")
     if concat:
         dataset = TensorDataset(
             torch.cat((s2_r_patches.float(), s2_a_patches.float()), axis=1)
@@ -651,33 +463,6 @@ def get_loader_from_patches(
             shuffle=shuffle,
         )
     return loader
-
-
-def get_bands_norm_factors_from_loaders(
-    loader, bands_dim=1, max_samples=10000, n_bands=10, mode="mean"
-):
-    s2_r_samples = torch.tensor([])
-    n_samples = 0
-    with torch.no_grad():
-        for i, batch in enumerate(loader):
-            s2_r = batch[0]
-            assert s2_r.size(bands_dim) == n_bands
-            s2_r = s2_r.transpose(0, bands_dim).reshape(n_bands, -1)
-            n_samples += s2_r.size(1)
-            s2_r_samples = torch.cat((s2_r_samples, s2_r), axis=1)
-            if n_samples >= max_samples:
-                break
-        if s2_r_samples.size(1) > max_samples:
-            s2_r_samples = s2_r_samples[:, :max_samples]
-        if mode == "mean":
-            norm_mean = s2_r_samples.mean(1)
-            norm_std = s2_r_samples.std(1)
-        elif mode == "quantile":
-            norm_mean = torch.quantile(s2_r_samples, q=torch.tensor(0.5), dim=1)
-            norm_std = torch.quantile(
-                s2_r_samples, q=torch.tensor(0.95), dim=1
-            ) - torch.quantile(s2_r_samples, q=torch.tensor(0.05), dim=1)
-    return norm_mean, norm_std
 
 
 if __name__ == "__main__":

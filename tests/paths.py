84d96d5e03c14f7323babd862908bfe96fcfdc9b
Jordi Inglada <jordi.inglada@cesbio.eu>
2024-03-26 10:49:14 +0100

    TEST: use Path instead of str

--- tests/paths.py
+++ tests/paths.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
-ROOT_DIR = "/work/CESBIO/projects/DeepChange/PROSAILVAE"
+ROOT_DIR = Path("/work/CESBIO/projects/DeepChange/PROSAILVAE")
+TMP_DIR = Path("/tmp")
 if Path("/usr/local/stok/DATA/MMDC").exists():
-    ROOT_DIR = "/usr/local/stok/DATA/MMDC/ProsailVAE/PROSAILVAE"
+    ROOT_DIR = Path("/usr/local/stok/DATA/MMDC/ProsailVAE/PROSAILVAE")
 elif Path("/gpfsscratch/rech/adz/uzh16pa/ProsailVAE").exists():
-    ROOT_DIR = "/gpfsscratch/rech/adz/uzh16pa/ProsailVAE/PROSAILVAE"
+    ROOT_DIR = Path("/gpfsscratch/rech/adz/uzh16pa/ProsailVAE/PROSAILVAE")
+    TMP_DIR = Path("/gpfsscratch/rech/adz/uzh16pa/tmp")
 PATCHES_DIR = Path(f"{ROOT_DIR}/s2_patch_dataset/")

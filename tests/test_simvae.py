963f21805fd1eee59bb20db6f7bcf24916b7e793
DE SOUSA Kevin <kevin.de-sousa@univ-tlse3.fr>
2024-04-05 15:23:28 +0200

    TEST: Set manual seed

--- tests/test_simvae.py
+++ tests/test_simvae.py
@@ -1,14 +1,15 @@
 from pathlib import Path
 
 import torch
+from einops import rearrange
 
 from prosailvae.encoders import EncoderConfig
 from prosailvae.loss import LossConfig
 from prosailvae.prosail_vae import ProsailVAEConfig, get_prosail_vae
 from prosailvae.utils.utils import load_standardize_coeffs
-import os
+
 from .paths import PATCHES_DIR
-from einops import rearrange
+
 SRC_DIR = Path(__file__).parent.parent
 RSR_DIR = SRC_DIR / "data"
 N_PROSAIL_VARS = 11
@@ -37,7 +38,6 @@ def test_instantiate():
     assert model is not None
 
 
-
 def test_forward():
     batch_size = 2
     patch_size = 4
@@ -46,34 +46,54 @@ def test_forward():
     data = torch.rand(batch_size, 10, patch_size, patch_size).to(DEVICE)
     angles = torch.rand(batch_size, 3, patch_size, patch_size).to(DEVICE)
     dist_params, z, phi, rec = model.forward(data, angles=angles, n_samples=3)
-    
+
     rec.sum().backward()
 
-def test_regression_pvae_method(): 
-    batch_size = 2
-    patch_size = 4
-    config = generate_config()
-    model = get_prosail_vae(config, DEVICE)
-    data = torch.rand(batch_size, 10, patch_size, patch_size).to(DEVICE)
-    angles = torch.rand(batch_size, 3, patch_size, patch_size).to(DEVICE)
-    dist_params_reg, z_reg, phi_reg, rec_reg = model.forward(data, angles=angles, n_samples=3)
-    dist_params, z, phi, rec = model.pvae_method([data, angles] , n_samples=3)
-    z_reg = rearrange(z_reg, 'batch samples bands w h -> (batch  w  h) bands samples').to(DEVICE)
-    rec_reg = rearrange( rec_reg, 'batch samples bands w h -> (batch  w  h) bands samples').to(DEVICE)
-    assert torch.eq(dist_params_reg, dist_params).all()
-    assert torch.isclose(rec_reg, rec,rtol =5).all()
 
-def test_pvae_loss(): 
+def test_regression_pvae_method():
+    batch_size = 2
+    patch_size = 1
+    config = generate_config()
+    model = get_prosail_vae(config, DEVICE)
+    data = torch.rand(batch_size, 10, patch_size, patch_size).to(DEVICE)
+    angles = torch.rand(batch_size, 3, patch_size, patch_size).to(DEVICE)
+    torch.manual_seed(42)
+    dist_params_reg, z_reg, phi_reg, rec_reg = model.forward(
+        data, angles=angles, n_samples=3
+    )
+    torch.manual_seed(42)
+    dist_params, z, phi, rec = model.pvae_method([data, angles], n_samples=3)
+    z_reg = rearrange(
+        z_reg, "batch samples bands w h -> (batch  w  h) bands samples"
+    ).to(DEVICE)
+    rec_reg = rearrange(
+        rec_reg, "batch samples bands w h -> (batch  w  h) bands samples"
+    ).to(DEVICE)
+
+    assert torch.eq(dist_params_reg, dist_params).all()
+    assert torch.eq(z_reg, z).all()
+    assert torch.eq(rec_reg, rec).all()
+
+
+def test_pvae_loss():
     batch_size = 2
     patch_size = 4
     config = generate_config()
     model = get_prosail_vae(config, DEVICE)
     data = torch.rand(batch_size, 10, patch_size, patch_size).to(DEVICE)
     angles = torch.rand(batch_size, 3, patch_size, patch_size).to(DEVICE)
-    dist_params, z, phi, rec = model.pvae_method([data, angles] , n_samples=20)
-    dic= {}
-    loss_sum, normalized_loss_dict = model.unsupervised_batch_loss([data,angles], dic, n_samples=20 )
-    kl_loss = model.pvae_kl_term( data, angles, dist_params)
-    assert kl_loss is not None 
-    rec_loss = model.pvae_reconstruction_loss( rearrange( data, 'b bands p p2 -> (b  p  p2) bands'), angles, z, rec)
-    assert torch.isclose(rec_loss.to(DEVICE),torch.tensor(normalized_loss_dict['rec_loss']).to(DEVICE),rtol=1e4 )
+    dist_params, z, phi, rec = model.pvae_method([data, angles], n_samples=20)
+    dic = {}
+    loss_sum, normalized_loss_dict = model.unsupervised_batch_loss(
+        [data, angles], dic, n_samples=20
+    )
+    kl_loss = model.pvae_kl_term(data, angles, dist_params)
+    assert kl_loss is not None
+    rec_loss = model.pvae_reconstruction_loss(
+        rearrange(data, "b bands p p2 -> (b  p  p2) bands"), angles, z, rec
+    )
+    assert torch.isclose(
+        rec_loss.to(DEVICE),
+        torch.tensor(normalized_loss_dict["rec_loss"]).to(DEVICE),
+        rtol=1e4,
+    )
